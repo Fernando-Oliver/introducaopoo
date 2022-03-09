c3739f267a5a7c7a81135c8725abbadb85372e1d
Fernando de Oliveira Carvalho <90574458+Fernando-Oliver@users.noreply.github.com>
2022-03-09 13:53:44 -0300

    Add files via upload

---
+++ poliformismo/Cachorro.java
@@ -0,0 +1,22 @@
+package poliformismo;
+
+public class Cachorro extends Animal {
+
+	 public void nome() {
+			System.out.println("Beethoven");
+		}
+	      public void idade() {
+	  		
+	  		System.out.println("2");
+	  	}
+		
+		public void emitirsom() {
+			
+			System.out.println("auauauauau");
+		}
+		
+	    public void acao() {
+			
+			System.out.println("correndo...");
+		}
+}
---
+++ poliformismo/Cavalo.java
@@ -0,0 +1,24 @@
+package poliformismo;
+
+public class Cavalo extends Animal{
+
+	 public void nome() {
+			System.out.println("Pé de Pano");
+		}
+	      public void idade() {
+	  		
+	  		System.out.println("3");
+	  	}
+		
+		public void emitirsom() {
+			
+			System.out.println("iiirrrrí");
+		}
+		
+	    public void acao() {
+			
+			System.out.println("correndo...");
+		}
+	
+
+}
